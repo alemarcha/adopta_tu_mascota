58ac4b2885a446451288b98fe083d5ab40ce842e
Alexis Martínez <alemarcha26@gmail.com>
2015-12-28 23:10:14 +0100

    Nuevo estilos paginación

--- client/es/alemarcha/mascota/main/controller/centralCtrl.js
+++ client/es/alemarcha/mascota/main/controller/centralCtrl.js
@@ -1,11 +1,23 @@
 (function () {
+    
+    
     var centralCtrl = function ($scope, $routeParams, centralFactory) {
+        
+  
         var vm = this;
+        vm.currentPage = 1;
+        vm.pageSize = 5;
         vm.usuario = {};
         var value=centralFactory.query(function(){
             console.log(value);
             vm.items=value.elementos;
         });   
+        
+        vm.pageChangeHandler = function(num) {
+            console.log('drinks page changed to ' + num);
+        };
     }
+    
+
     angular.module('adoptaTuMascotaApp').controller('CentralCtrl', centralCtrl);
 }());
