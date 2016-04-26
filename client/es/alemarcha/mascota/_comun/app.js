a58d92f96dbc167a16b6405263bd26a8b0f945f1
Alexis Martínez <alemarcha26@gmail.com>
2016-04-26 20:41:44 +0200

    Fechas en elementos con timezone y locale. Reestructurando la pantalla y añandiendo social share.

--- client/es/alemarcha/mascota/_comun/app.js
+++ client/es/alemarcha/mascota/_comun/app.js
@@ -1,103 +1,107 @@
-angular.module('adoptaTuMascotaApp', ['ngRoute','ngResource','angularUtils.directives.dirPagination','angularSpinner','growlNotifications','satellizer','ngMap']);
+angular.module('adoptaTuMascotaApp', ['ngRoute', 'ngResource', 'angularUtils.directives.dirPagination', 'angularSpinner', 'growlNotifications', 'satellizer', 'ngMap', 'angularMoment', '720kb.socialshare']);
 
 angular.module('adoptaTuMascotaApp').config(['$routeProvider', '$authProvider',
-  function($routeProvider,$authProvider) {
-            
-    // No additional setup required for Twitter
-$authProvider.httpInterceptor = function() { return true; },
-$authProvider.baseUrl = '/api';
-$authProvider.loginUrl = '/auth/login';
-$authProvider.signupUrl = '/private/auth/register';
-$authProvider.tokenName = 'token';
-$authProvider.tokenPrefix = 'adoptaTuMascotaApp';
-$authProvider.authHeader = 'Authorization';
-$authProvider.authToken = 'Bearer';
-$authProvider.storageType = 'localStorage';
-      
-    // ROUTE
-    $routeProvider.
-    when('/login', {
-        templateUrl: 'registro/view/login.html',
-        controller: 'RegistroCtrl',
-        controllerAs: 'registro'
-    }).
-    when('/element/:id', {
-        templateUrl: 'element/view/element.html',
-        controller: 'ElementCtrl',
-        controllerAs: 'element'
-    }).
-     when('/new', {
-        templateUrl: 'altaElemento/view/new.html',
-        controller: 'NewElementCtrl',
-        access: {restricted: true},
-        controllerAs: 'newElement'
-    }).
-    when('/404', {
-        templateUrl: 'error/404/404.html'
-    }).
-    when('/401', {
-        templateUrl: 'error/401/401.html'
-    }).
-    otherwise({
-        templateUrl: 'main/view/central.html',
-        controller: 'CentralCtrl',
-        controllerAs: 'central'
+  function ($routeProvider, $authProvider) {
+
+        // No additional setup required for Twitter
+        $authProvider.httpInterceptor = function () {
+                return true;
+            },
+            $authProvider.baseUrl = '/api';
+        $authProvider.loginUrl = '/auth/login';
+        $authProvider.signupUrl = '/private/auth/register';
+        $authProvider.tokenName = 'token';
+        $authProvider.tokenPrefix = 'adoptaTuMascotaApp';
+        $authProvider.authHeader = 'Authorization';
+        $authProvider.authToken = 'Bearer';
+        $authProvider.storageType = 'localStorage';
+
+        // ROUTE
+        $routeProvider.
+        when('/login', {
+            templateUrl: 'registro/view/login.html',
+            controller: 'RegistroCtrl',
+            controllerAs: 'registro'
+        }).
+        when('/element/:id', {
+            templateUrl: 'element/view/element.html',
+            controller: 'ElementCtrl',
+            controllerAs: 'element'
+        }).
+        when('/new', {
+            templateUrl: 'altaElemento/view/new.html',
+            controller: 'NewElementCtrl',
+            access: {
+                restricted: true
+            },
+            controllerAs: 'newElement'
+        }).
+        when('/404', {
+            templateUrl: 'error/404/404.html'
+        }).
+        when('/401', {
+            templateUrl: 'error/401/401.html'
+        }).
+        otherwise({
+            templateUrl: 'main/view/central.html',
+            controller: 'CentralCtrl',
+            controllerAs: 'central'
+        });
+
+
+  }]);
+
+angular.module('adoptaTuMascotaApp').run(['$rootScope', '$location', '$route', 'authFactory', '$auth', function ($rootScope, $location, $route, authFactory, $auth) {
+    $rootScope.$on('$routeChangeStart', function (e, curr, prev) {
+
+        // Show a loading message until promises aren't resolved
+        $rootScope.loading = true;
+
+
+        if ($auth.isAuthenticated()) {
+
+            authFactory.isAuthenticated().then(function (data) {
+                if (curr.access && curr.access.restricted) {
+
+                    if (!$rootScope.auth === true) {
+                        $location.path('/login');
+                        $route.reload();
+
+                    } else {
+                        alert("adelante");
+                    }
+                }
+            }, function (data) {
+
+                // Since this segment of the promise signals that the user is not
+                // logged in, if the page is not publicly accessible, redirect to login
+
+                $location.path('/login');
+
+            }).catch(function (response) {
+                $location.path('/login');
+            });
+
+        } else {
+            if (curr.access && curr.access.restricted) {
+                //eliminaVariablesLogueoSession();
+                $location.path('/login');
+            }
+        }
+
     });
-
-      
   }]);
 
-angular.module('adoptaTuMascotaApp').run(['$rootScope', '$location','$route','authFactory','$auth', function($rootScope, $location, $route,authFactory,$auth) {
-     $rootScope.$on('$routeChangeStart', function(e, curr, prev) { 
-    
-      // Show a loading message until promises aren't resolved
-        $rootScope.loading=true;
 
-         
-         if($auth.isAuthenticated()){
-                
-         authFactory.isAuthenticated().then(function(data){
-            if (curr.access && curr.access.restricted){
-             
-                 if(!$rootScope.auth===true) {
-                         $location.path('/login');
-                         $route.reload();
 
-                 }else{
-                     alert("adelante");
-                 }
-             }
-         }, function(data) {
 
-        // Since this segment of the promise signals that the user is not
-        // logged in, if the page is not publicly accessible, redirect to login
-        
-            $location.path('/login');
 
-         }).catch(function(response) {
-                    $location.path('/login');
-         });  
-
-     }else{
-         if (curr.access && curr.access.restricted){
-         //eliminaVariablesLogueoSession();
-      $location.path('/login');
-         }
-     }
-         
-     });
-  }]);
-    
-  
-
- 
-
-    angular.module('adoptaTuMascotaApp').run(
+angular.module('adoptaTuMascotaApp').run(
     function ($rootScope) {
         $rootScope.loading = false;
-        $rootScope.showGrowl= false;
-        $rootScope.indexNotificacion=0;
-        $rootScope.notifications={};
+        $rootScope.showGrowl = false;
+        $rootScope.indexNotificacion = 0;
+        $rootScope.notifications = {};
     });
 
 
--- client/es/alemarcha/mascota/element/controller/elementCtrl.js
+++ client/es/alemarcha/mascota/element/controller/elementCtrl.js
@@ -2,32 +2,47 @@
     angular
         .module('adoptaTuMascotaApp')
         .controller('ElementCtrl', elementCtrl);
-    
-    elementCtrl.$inject = ['$routeParams', 'elementFactory'];
-    
-    function elementCtrl ($routeParams, elementFactory) {
+
+    elementCtrl.$inject = ['$routeParams', 'elementFactory', 'moment'];
+
+    function elementCtrl($routeParams, elementFactory) {
         var vm = this;
-        vm.id=$routeParams.id;
+        vm.id = $routeParams.id;
         console.log(vm.id);
-        
+
         initialize();
-        
+
         console.log("empieza");
-        function initialize(){
-         elementFactory.elementById.query({id:vm.id},function(data){
-            if(data){
-                vm.elementoActual=data;
-                console.log(vm.elementoActual.titulo);
-                console.log("aqui");
-            }else{
-                
-            }
-        },function(error){
-             console.log("ERROR");
-             
-         }); 
+
+        function initialize() {
+            // Seteamos el timezone por defecto
+            moment.tz.setDefault("UTC");
+
+
+            elementFactory.elementById.query({
+                    id: vm.id
+                }, function (data) {
+                    if (data) {
+                        vm.elementoActual = data;
+
+                        // Cogemos el locale del navegador para poner el texto
+                        moment.locale(window.navigator.userLanguage || window.navigator.language);
+                        var testDateUtc = moment.utc(data.date);
+                        // Cogemos el date de BD en timezone UTC y lo convertimos a la hora local. Usamos la libreria jstz para obtener el timezone del browser.
+                        vm.elementoActual.fromNow = testDateUtc.tz(jstz.determine().name()).fromNow();
+                        vm.elementoActual.dateFormat = testDateUtc.tz(jstz.determine().name()).format('DD/MM/YYYY HH:mm:ss');
+                    } else {
+
+                    }
+                },
+                function (error) {
+                    console.log("ERROR");
+
+                });
         }
     }
-    
-    
+
+
 }());
+
+// HAY QUE HACER FUNCIONAR BIEN LA HORA CON EL TIMEZONE SEGUN LAS ZONA EN LA QUE ESTES
